c9736091d3e6f198b2ef565348ab7f257145f3d1
ICanSayNo <league@WTS20.attlocal.net>
2017-11-29 18:30:08 -0800

    sound track,String methods 1 and 2,NASA countdown

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -11,13 +11,21 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "What is your mood?", "MOOOOOOOOODDDDDDSSSSSS", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Happy", "sad", "mad" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
+		if(userMood==2) {
+playVideo("http://bit.ly/video-for-angry");
+		}
+if(userMood==1) {
+	playVideo(	"http://bit.ly/video-for-sad");
+}
+	if(userMood==0) {
+	playVideo("http://bit.ly/video-for-happy");
+		}
 
 		
 		// 4. Play different songs for other moods.
@@ -26,7 +34,7 @@ public class SoundtrackToLife {
 * If you can’t think of any, here are some you can use...
 *	http://bit.ly/video-for-happy
 *	http://bit.ly/video-for-sad
-*	http://bit.ly/video-for-angry
+*	
 **/
 		
 		// If you are seeing ads at the beginning of your videos, install Adblock.
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -7,19 +7,22 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+String a= "VfgdRYJhgfG";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(a.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+System.out.println(a.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+		System.out.println(a.substring(0,3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(a.substring(5,6));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+	int end=a.length();
+	int start=end-3;
+		System.out.println(a.substring(start,end));
 	}
 }
 
